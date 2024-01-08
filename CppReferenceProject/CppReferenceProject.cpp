bc5cc2a4847f29ae7a48781002b5740d5217bd04
Максим Ефимов <efimov_m@t.top-academy.ru>
2024-01-08 21:21:01 +0300

    Добавьте файлы проекта.

---
+++ CppReferenceProject/CppReferenceProject.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+
+void func(int& b)
+{
+    b = 100;
+}
+
+int main()
+{
+    int a{ 10 };
+
+    std::cout << a << " " << &a << "\n";
+
+    /*int& b{ a };
+    b = 20;*/
+
+    func(a);
+
+    std::cout << a << " " << &a << "\n";
+
+    int* ptr = &a;
+
+    std::cout << ptr << " " << &ptr << "\n";
+
+}
